3f9c8b260889c29e1e32609a686b5069ba6cd85b
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-05-10 15:18:59 -0500

    Rewrote service file

--- lua/true-zen/services/mode-minimalist/service.lua
+++ lua/true-zen/services/mode-minimalist/service.lua
@@ -1,22 +1,19 @@
-local bottom = require("true-zen.services.bottom.init")
-local top = require("true-zen.services.top.init")
-local left = require("true-zen.services.left.init")
+local M = {}
+
+local tz_services = require("true-zen.services")
 
 -- bottom specific options
 
-function minimalist_true() -- show
-	bottom.main(1)
-	top.main(1)
-	left.main(1)
+function M.minimalist_true() -- show
+	tz_services.bottom.main("enable")
+	tz_services.top.main("enable")
+	tz_services.left.main("enable")
 end
 
-function minimalist_false() -- don't show
-	bottom.main(2)
-	top.main(2)
-	left.main(2)
+function M.minimalist_false() -- don't show
+	tz_services.bottom.main("disable")
+	tz_services.top.main("disable")
+	tz_services.left.main("disable")
 end
 
-return {
-	minimalist_true = minimalist_true,
-	minimalist_false = minimalist_false,
-}
+return M
