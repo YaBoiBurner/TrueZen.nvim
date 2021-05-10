087b221be1f98a7f4fdde6e23bae8fe70981e8fc
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-05-10 17:18:06 -0500

    Added module table to service

--- lua/true-zen/services/mode-focus/service.lua
+++ lua/true-zen/services/mode-focus/service.lua
@@ -1,26 +1,23 @@
+local M = {}
+
 -- bottom specific options
 
-local function native_focus_true() -- show
+function M.native_focus_true() -- show
 	vim.cmd("vert resize | resize")
 	vim.cmd("normal! ze")
 end
 
-local function native_focus_false() -- don't show
+function M.native_focus_false() -- don't show
 	vim.cmd("wincmd =")
 	vim.cmd("normal! ze")
 end
 
-local function experimental_focus_true()
+function M.experimental_focus_true()
 	vim.cmd("tabe %")
 end
 
-local function experimental_focus_false()
+function M.experimental_focus_false()
 	vim.cmd("q")
 end
 
-return {
-	native_focus_true = native_focus_true,
-	native_focus_false = native_focus_false,
-	experimental_focus_true = experimental_focus_true,
-	experimental_focus_false = experimental_focus_false,
-}
+return M
