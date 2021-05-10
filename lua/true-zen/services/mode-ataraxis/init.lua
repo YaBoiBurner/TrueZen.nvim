240a11787e4589e0b8d354b16f876e2e7483d918
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-05-10 17:59:27 -0500

    Cleaned up ataraxis init

--- lua/true-zen/services/mode-ataraxis/init.lua
+++ lua/true-zen/services/mode-ataraxis/init.lua
@@ -1,36 +1,37 @@
-local service = require("true-zen.services.mode-ataraxis.service")
+local M = {}
 
-local cmd = vim.cmd
-local api = vim.api
+M.ataraxis_show = nil
+
+local service = require("true-zen.services.mode-ataraxis.service")
 
 -- show and hide ataraxis funcs
 local function ataraxis_true()
-	ataraxis_show = 1
+	M.ataraxis_show = 1
 	service.ataraxis_true()
 end
 
 local function ataraxis_false()
-	ataraxis_show = 0
+	M.ataraxis_show = 0
 	service.ataraxis_false()
 end
 
 -- 1 if being shown
 -- 0 if being hidden
 local function toggle()
-	if ataraxis_show == 1 then -- ataraxis true, shown; thus, hide
+	if M.ataraxis_show == 1 then -- ataraxis true, shown; thus, hide
 		ataraxis_false()
-	elseif ataraxis_show == 0 then -- ataraxis false, hidden; thus, show
+	elseif M.ataraxis_show == 0 then -- ataraxis false, hidden; thus, show
 		ataraxis_true()
-	elseif ataraxis_show == nil then
-		ataraxis_show = 1
+	elseif M.ataraxis_show == nil then
+		M.ataraxis_show = 1
 		ataraxis_false()
 	else
-		ataraxis_show = 1
+		M.ataraxis_show = 1
 		ataraxis_false()
 	end
 end
 
-function main(option)
+function M.main(option)
 	option = option or 0
 
 	if option == 0 then -- toggle statuline (on/off)
@@ -39,12 +40,7 @@ function main(option)
 		ataraxis_true()
 	elseif option == 2 then
 		ataraxis_false()
-	else
-		-- not recognized
 	end
 end
 
-return {
-	main = main,
-	ataraxis_show = ataraxis_show,
-}
+return M
