3547df6f732f397a8482434f208b87ace0148a54
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-05-10 19:01:43 -0500

    Rewrote rest of hi_group.lua (no more VimL function definitions)

--- lua/true-zen/services/mode-ataraxis/modules/hi_group.lua
+++ lua/true-zen/services/mode-ataraxis/modules/hi_group.lua
@@ -12,63 +12,41 @@ local terms = {
 	"gui",
 }
 
-local cmd = vim.cmd
+local function has(...)
+	return vim.fn.has(...) == 1
+end
+
+local function GetColor(group, attr)
+	return vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(group)), attr)
+end
+
+local function SetColor(group, attr, color)
+	local gui = has("gui_running") or (has("termguicolors") and vim.o.termguicolors)
+	vim.cmd(string.format("hi %s %s%s=%s", group, (gui and "gui" or "cterm"), attr, color))
+end
+
+local function Tranquilize(bg_color)
+	local bg = GetColor("Normal", "bg#")
+	for _, grp in ipairs({ "NonText", "FoldColumn", "ColorColumn", "VertSplit", "StatusLine", "StatusLineNC", "SignColumn" }) do
+		-- -1 on Vim / '' on GVim
+		if bg == -1 or bg == "" then
+			SetColor(grp, "fg", bg_color)
+			SetColor(grp, "bg", "NONE")
+		else
+			SetColor(grp, "fg", bg)
+			SetColor(grp, "bg", bg)
+		end
+	end
+end
 
 function M.set_hi_groups(custome_bg)
 	custome_bg = custome_bg or ""
 
-	vim.api.nvim_exec(
-		[[
-		function! GetColor(group, attr)
-			return synIDattr(synIDtrans(hlID(a:group)), a:attr)
-		endfunction
-		com! -nargs=+ -complete=command GetColor call GetColor(<q-args>)
-
-	]],
-		false
-	)
-
-	vim.api.nvim_exec(
-		[[
-		function! SetColor(group, attr, color)
-			let gui = has('gui_running') || has('termguicolors') && &termguicolors
-			execute printf('hi %s %s%s=%s', a:group, gui ? 'gui' : 'cterm', a:attr, a:color)
-		endfunction
-		com! -nargs=+ -complete=command SetColor call SetColor(<q-args>)
-	]],
-		false
-	)
-
-	vim.api.nvim_exec(
-		[[
-		function! Tranquilize(bg_color)
-			let bg = GetColor('Normal', 'bg#')
-			for grp in ['NonText', 'FoldColumn', 'ColorColumn', 'VertSplit', 'StatusLine', 'StatusLineNC', 'SignColumn']
-				" -1 on Vim / '' on GVim
-				if bg == -1 || empty(bg)
-					call SetColor(grp, 'fg', a:bg_color)
-					call SetColor(grp, 'bg', 'NONE')
-				else
-					call SetColor(grp, 'fg', bg)
-					call SetColor(grp, 'bg', bg)
-				endif
-
-				call SetColor(grp, '', 'NONE')
-			endfor
-		endfunction
-	]],
-		false
-	)
-
-	local call_tran = ""
-
 	if custome_bg == "" or custome_bg == nil then
-		call_tran = "call Tranquilize('black')"
+		Tranquilize("black")
 	else
-		call_tran = "call Tranquilize('" .. custome_bg .. "')"
+		Tranquilize(custome_bg)
 	end
-
-	cmd(call_tran)
 end
 
 function M.store_hi_groups()
